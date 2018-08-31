d54eba636fb0b7eb5d42d740b5041edb73214090
Kyle Meserve <KyleMeserve@Kyles-MacBook-Air.local>
2018-08-31 11:18:25 -0400

    finished game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -12,7 +12,7 @@ cardImage: "images/queen-of-diamonds.png"
 {
 rank: "king",
 suit: "hearts",
-cardImage: "iamges/king-of-hearts.png"
+cardImage: "images/king-of-hearts.png"
 }, 
 {
 rank: "king",
@@ -33,10 +33,21 @@ var checkForMatch = function() {
 	}
 }
 
-var flipCard = function(cardId) {
-	console.log("User flipped " + cards[cardId].rank);
+var flipCard = function() {
+	var cardId = this.getAttribute('data-id');
 	cardsInPlay.push(cards[cardId].rank);
-	console.log(cards[cardId].suit);
-	console.log(cards[cardId].cardImage);
+	this.setAttribute('src', cards[cardId].cardImage);
 	checkForMatch();
 }
+
+var createBoard = function() {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+}
+
+createBoard();
